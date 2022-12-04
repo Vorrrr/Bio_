ecf170e1989aea0a66f3fdddfbef3a4e64026fc6
Vorrrr <119088454+Vorrrr@users.noreply.github.com>
2022-12-05 02:51:05 +0300

    Add files via upload

---
+++ rna_counter.py
@@ -0,0 +1,21 @@
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+
+
+total_pr = 0
+total_rna = 0
+file = open('C:\Надя\GCF_013343195.2_ASM1334319v3_feature_table.txt', 'r')
+for i in file.readlines():
+    i = i.split('\t')
+    gene = i[1]
+    if gene == 'with_protein':
+        total_pr += 1
+    if 'RNA' in gene:
+        total_rna += 1
+        
+file.close()
+print('RNA total: ', total_rna, '\nRNA/protein = ', str(round(total_rna/total_pr, 5)))
+
+
