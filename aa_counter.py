939d20014c063c504510c8d5b7688d4cbb594965
Vorrrr <119088454+Vorrrr@users.noreply.github.com>
2022-12-05 01:12:13 +0300

    Add files via upload

--- aa_counter.py
+++ aa_counter.py
@@ -4,15 +4,23 @@ import matplotlib.pyplot as plt
 
 
 aa_count = []
+A = False
 file = open('C:\Надя\GCF_013343195.2_ASM1334319v3_feature_table.txt', 'r')
 for i in file.readlines():
     i = i.split('\t')
     gene = i[1]
     if gene == 'protein_coding':
         aa_count.append(int(i[17])// 3)
+        if int(i[17])// 3 > 2000:
+            A = True
+    if A and gene == 'with_protein':
+        print(i[13], '; lenght: ', aa_count[-1], sep='')
+        A = False
 file.close()
 data = pd.DataFrame(data=aa_count)
 h = data.hist(legend=False, bins=70)
-plt.show()
-print(h)
+print('Средняя длина белков:', int(data.mean()))
+print('Медиана длины белков:', int(data.median()))
+print('Мода длин белков:', int(data.median()))
 
+plt.show()
