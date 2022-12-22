e0d86f1fd615349495e7ea653b6c630cfdd9db3f
Vorrrr <119088454+Vorrrr@users.noreply.github.com>
2022-12-22 16:23:46 +0300

    Update protein_counter.py

--- protein_counter.py
+++ protein_counter.py
@@ -1,5 +1,5 @@
 minus, plus, protein_counter = 0, 0, 0
-file = open('C:\Надя\GCF_013343195.2_ASM1334319v3_feature_table.txt', 'r')
+file = open(input(), 'r')
 for i in file.readlines():
     strain, gene = i.split('\t')[9], i.split('\t')[1]
     if gene == 'with_protein':
