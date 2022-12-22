b4b91c9ea52c048e72335cce14efbf6dbbaf99da
Vorrrr <119088454+Vorrrr@users.noreply.github.com>
2022-12-22 16:25:24 +0300

    Update types_rna_counter.py

--- types_rna_counter.py
+++ types_rna_counter.py
@@ -2,7 +2,7 @@ r_rna = 0
 t_rna = 0
 oth = ''
 total = 0
-file = open('C:\Надя\GCF_013343195.2_ASM1334319v3_feature_table.txt', 'r')
+file = open(input(), 'r')
 for i in file.readlines():
     i = i.split('\t')
     gene = i[1]
